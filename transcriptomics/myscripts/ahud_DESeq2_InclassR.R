b84737fc038b7aa91cffbb7e4d448eedeada39b8
Julian Drezner <jdrezner@node313.cluster>
2026-09-24 16:30:22 -0400

    practice

---
+++ transcriptomics/myscripts/ahud_DESeq2_InclassR.R
@@ -0,0 +1,235 @@
+###this is my r.script for exploring
+###using DESeq2
+
+
+## Set your working directory
+setwd("~/Projects/eco_genomics_2026/transcriptomics")
+
+## Import the libraries that we're likely to need in this session
+
+library(DESeq2)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(scales)
+library(ggpubr)
+library(vsn)  
+library("pheatmap")
+library("vsn")
+
+####################################################
+
+### Import our data
+
+####################################################
+
+
+# Import the counts matrix
+countsTable <- read.table("mydata/salmon.isoform.counts.matrix.filteredAssembly", header=TRUE, row.names=1)
+head(countsTable)
+dim(countsTable)
+
+countsTableRound <- round(countsTable) # bc DESeq2 doesn't like decimals (and Salmon outputs data with decimals)
+head(countsTableRound)
+
+#import the sample description table
+conds <- read.delim("mydata/ahud_samples_R.txt", header=TRUE, stringsAsFactors = TRUE, row.names=1)
+head(conds)
+
+####################################################
+
+### Explore data distributions
+
+####################################################
+
+# Let's see how many reads we have from each sample
+colSums(countsTableRound)
+mean(colSums(countsTableRound))
+
+barplot(colSums(countsTableRound), names.arg=colnames(countsTableRound),cex.names=0.5, las=3,ylim=c(0,21000000))
+abline(h=mean(colSums(countsTableRound)), col="blue", lwd=2)
+
+# the average number of counts per gene
+rowSums(countsTableRound)
+mean(rowSums(countsTableRound)) # [1] 8217.81
+median(rowSums(countsTableRound)) # [1] 377
+
+apply(countsTableRound,2,mean) # 2 in the apply function does the action across columns
+apply(countsTableRound,1,mean) # 1 in the apply function does the action across rows
+hist(apply(countsTableRound,1,mean),xlim=c(0,10000), ylim=c(0,60000),breaks=10000)
+
+####################################################
+
+### Start working with DESeq2!
+
+####################################################
+
+#### Create a DESeq object and define the experimental design here with the tilda
+
+dds <- DESeqDataSetFromMatrix(countData = countsTableRound, colData=conds, 
+                              design= ~ generation + treatment)
+
+dim(dds)
+
+# Filter out genes with too few reads - remove all genes with counts < 15 in more than 75% of samples, so ~28)
+## suggested by WGCNA on RNAseq FAQ
+
+dds <- dds[rowSums(counts(dds) >= 15) >= 28,]
+nrow(dds) 
+# [1] 25260, that have at least 15 reads (a.k.a counts) in 75% of the samples
+
+# Run the DESeq model to test for differential gene expression
+dds <- DESeq(dds)
+
+# List the results you've generated
+resultsNames(dds)
+# Copy the results names 
+
+# The goal of transformation "is to remove the dependence of the variance on the mean, particularly the high variance of the logarithm of count data when the mean is low."
+
+# this gives log2(n + 1)
+ntd <- normTransform(dds)
+meanSdPlot(assay(ntd))
+
+# Variance stabilizing transformation
+vsd <- vst(dds, blind=FALSE)
+meanSdPlot(assay(vsd))
+
+sampleDists <- dist(t(assay(vsd)))
+
+library("RColorBrewer")
+sampleDistMatrix <- as.matrix(sampleDists)
+rownames(sampleDistMatrix) <- paste(vsd$line, vsd$generation, sep="-")
+colnames(sampleDistMatrix) <- NULL
+colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+pheatmap(sampleDistMatrix,
+         clustering_distance_rows=sampleDists,
+         clustering_distance_cols=sampleDists,
+         col=colors)
+
+# Note any outliers maybe from AH (OA) f2 rep 2
+
+sampleTree <- hclust(dist(sampleDists), method="average")
+# plot
+plot(sampleTree, main="Sample clustering to detect outliers", sub="", xlab="",cex.lab=1.5, cex.axis=1.5, cex.main=2)
+
+# first transform the data for plotting using variance stabilization
+vsd <- vst(dds, blind=FALSE)
+
+pcaData <- plotPCA(vsd, intgroup=c("treatment","generation"), returnData=TRUE)
+percentVar <- round(100 * attr(pcaData,"percentVar"))
+
+ggplot(pcaData, aes(PC1, PC2, color=treatment, shape=generation)) +
+  geom_point(size=3) +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
+  coord_fixed()
+
+###############################################################
+
+# Let's plot the PCA by generation in four panels
+
+data <- plotPCA(vsd, intgroup=c("treatment","generation"), returnData=TRUE)
+percentVar <- round(100 * attr(data,"percentVar"))
+
+###########  
+
+dataF0 <- subset(data, generation == 'F0')
+
+F0 <- ggplot(dataF0, aes(PC1, PC2)) +
+  geom_point(size=10, stroke = 1.5, aes(fill=treatment, shape=treatment)) +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+  ylim(-10, 25) + xlim(-40, 10)+ # zoom for F0 with new assembly
+  scale_shape_manual(values=c(21,22,23,24), labels = c("Ambient", "Acidification","Warming", "OWA"))+
+  scale_fill_manual(values=c('#6699CC',"#F2AD00","#00A08A", "#CC3333"), labels = c("Ambient", "Acidification","Warming", "OWA"))+
+  theme_bw() +
+  theme(legend.position = "none") +
+  theme(panel.border = element_rect(color = "black", fill = NA, size = 4))+
+  theme(text = element_text(size = 20)) +
+  theme(legend.title = element_blank())
+
+F0
+
+
+################# F2
+
+dataF2 <- subset(data, generation == 'F2')
+
+F2 <- ggplot(dataF2, aes(PC1, PC2)) +
+  geom_point(size=10, stroke = 1.5, aes(fill=treatment, shape=treatment)) +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+  ylim(-40, 25) + xlim(-50, 55)+
+  scale_shape_manual(values=c(21,22,23), labels = c("Ambient", "Acidification","Warming"))+
+  scale_fill_manual(values=c('#6699CC',"#F2AD00","#00A08A"), labels = c("Ambient", "Acidification","Warming"))+
+  theme(legend.position = c(0.83,0.85), legend.background = element_blank(), legend.box.background = element_rect(colour = "black")) +
+  guides(shape = guide_legend(override.aes = list(shape = c( 21,22, 23))))+
+  guides(fill = guide_legend(override.aes = list(shape = c( 21,22, 23))))+
+  guides(shape = guide_legend(override.aes = list(size = 5)))+
+  theme_bw() +
+  theme(legend.position = "none") +
+  theme(panel.border = element_rect(color = "black", fill = NA, size = 4))+
+  theme(text = element_text(size = 20)) +
+  theme(legend.title = element_blank())
+F2
+
+# Yes - F2 is missing one ambient replicate
+
+################################ F4
+
+dataF4 <- subset(data, generation == 'F4')
+
+F4 <- ggplot(dataF4, aes(PC1, PC2)) +
+  geom_point(size=10, stroke = 1.5, aes(fill=treatment, shape=treatment)) +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+  ylim(-40, 25) + xlim(-50, 55)+ # limits with filtered assembly
+  scale_shape_manual(values=c(21,22,23,24), labels = c("Ambient", "Acidification","Warming", "OWA"))+
+  scale_fill_manual(values=c('#6699CC',"#F2AD00","#00A08A", "#CC3333"), labels = c("Ambient", "Acidification","Warming", "OWA"))+
+  guides(shape = guide_legend(override.aes = list(shape = c( 21,22, 23, 24))))+
+  guides(fill = guide_legend(override.aes = list(shape = c( 21,22, 23, 24))))+
+  guides(shape = guide_legend(override.aes = list(size = 5)))+
+  theme_bw() +
+  theme(legend.position = "none") +
+  theme(panel.border = element_rect(color = "black", fill = NA, size = 4))+
+  theme(text = element_text(size = 20)) +
+  theme(legend.title = element_blank())
+F4
+
+
+################# F11
+
+dataF11 <- subset(data, generation == 'F11')
+
+F11 <- ggplot(dataF11, aes(PC1, PC2)) +
+  geom_point(size=10, stroke = 1.5, aes(fill=treatment, shape=treatment)) +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+  ylim(-45, 25) + xlim(-50, 55)+
+  scale_shape_manual(values=c(21,24), labels = c("Ambient", "OWA"))+
+  scale_fill_manual(values=c('#6699CC', "#CC3333"), labels = c("Ambient", "OWA"))+
+  guides(shape = guide_legend(override.aes = list(shape = c( 21, 24))))+
+  guides(fill = guide_legend(override.aes = list(shape = c( 21, 24))))+
+  guides(shape = guide_legend(override.aes = list(size = 5)))+
+  theme_bw() +
+  theme(legend.position = "none") +
+  theme(panel.border = element_rect(color = "black", fill = NA, size = 4))+
+  theme(text = element_text(size = 20)) +
+  theme(legend.title = element_blank())
+F11
+
+
+#("PCA_F11.png", res=300, height=5, width=5, units="in")
+# 
+# ggarrange(F11, nrow = 1, ncol=1)
+# 
+# dev.off()
+
+ggarrange(F0, F2, F4, F11, nrow = 2, ncol=2)
+
+png("./myresuts/PCA_allGens.png", res=300, height=5, width=5, units="in")
+
+ggarrange(F0, F2, F4, F11, nrow = 2, ncol=2)
+
+dev.off()
